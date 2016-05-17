fbbf543faf19f799299d067e9056c7733d69ca69
ShotaOda <shota.q3q2@gmail.com>
2016-05-17 17:40:48 +0900

    a

--- js/ReadReport/WorkGadget.View.ReadReport.js
+++ js/ReadReport/WorkGadget.View.ReadReport.js
@@ -11,6 +11,47 @@ var WorkGadget = WorkGadget || {};
 	// ------------------
 	WorkGadget.View.ReadReportRow = Backbone.Marionette.ItemView.extend({
 		template: '#template-ReadReportRow',
+		tagName: 'tr',
+		ui : {
+			"button" : "td"
+		},
+
+		events :{
+			"click @ui.button" : "showModal"
+		},
+
+		onRender : function(){
+			var $modal = $(
+				'<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="ModalLabel">\
+				<div class="modal-dialog modal-lg">\
+				<div class="modal-content">\
+				<div class="modal-header">\
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">\
+				<span aria-hidden="true">&times;</span>\
+				</button>\
+				<h4 class="modal-title" id="ModalLabel">' +
+				this.model.get("author") +
+				'</h4>\
+				</div>\
+				<div class="modal-body">\
+				</div>\
+				</div>\
+				</div>\
+				</div>'
+			)
+			this.$el.append($modal)
+		},
+
+		showModal : function(){
+			var content = this.model.get("content");
+			if (this.model.get("type") == "text") {
+				content = "<pre>" + content +  "</pre>"
+			}
+			this.$el.find(".modal-body")
+			.html(content)
+			this.$el.find(".modal").modal();
+		},
+
 	})
 
 	WorkGadget.View.ReadReportTable = Backbone.Marionette.CompositeView.extend({
@@ -18,8 +59,8 @@ var WorkGadget = WorkGadget || {};
 		template: '#template-ReadReportTable',
 
 		collectionEvents: {
-      change: 'render'
-    },
+			change: 'render'
+		},
 
 		childView: WorkGadget.View.ReadReportRow,
 		childViewContainer: '#MailRowContainer',
@@ -54,33 +95,33 @@ var WorkGadget = WorkGadget || {};
 
 		templateHelpers: function(){
 			return {
-				 todayStr: function(){
-				 	return WorkGadget.Common.fn.getYYYYMMDD(new Date())
-				 }
+				todayStr: function(){
+					return WorkGadget.Common.fn.getYYYYMMDD(new Date())
+				}
 				,
 			}
 		},
 
 		onShow: function () {
-	        // Invoke the datetimepicker plugin
-      	 	this.setCalendar();
-	    },
+			// Invoke the datetimepicker plugin
+			this.setCalendar();
+		},
 
 		onClose: function () {
-        	// Destroy the datetimepicker plugin
-        	this.ui.calendar.datepicker('destroy');
-    	},
+			// Destroy the datetimepicker plugin
+			this.ui.calendar.datepicker('destroy');
+		},
 
 		setCalendar: function(){
 			this.ui.calendar.datepicker({
-			    todayBtn: "linked",
-			    orientation: "bottom auto",
-			    keyboardNavigation: false,
-			    daysOfWeekDisabled: "0,6",
-			    autoclose: true,
-			    todayHighlight: true,
-			    format: "yyyy/mm/dd",
-			    endDate: "today",
+				todayBtn: "linked",
+				orientation: "bottom auto",
+				keyboardNavigation: false,
+				daysOfWeekDisabled: "0,6",
+				autoclose: true,
+				todayHighlight: true,
+				format: "yyyy/mm/dd",
+				endDate: "today",
 			});
 		},
 
